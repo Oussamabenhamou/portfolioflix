174a3ae9f284a52b1da623628bed496d53d42dbb
Aya_gh <aghiouan7@gmail.com>
2024-07-19 13:34:21 +0100

    first commit

--- interface/pages/_document.tsx
+++ interface/pages/_document.tsx
@@ -32,10 +32,26 @@ class MyDocument extends Document<DocumentProps> {
                 <meta name="description" content="Hi there! I'm El Kaimouni Abderrahmane, a dedicated Software Engineer and Data Scientist student passionate about crafting innovative solutions. Specializing in Web Development, skilled in both frontend and backend, I excel in DevOps practices, I'm proficient in deploying projects on various hosting environments, including VPS and cloud services, I bring a wealth of technical expertise to the table." />
 
                 <link rel="icon" href="/favicon.png" />
+                
+                <link rel="stylesheet" href="/assets/css/animate.css" />
+                <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
+                <link rel="stylesheet" href="/assets/bootstarp/bootstrap.min.css" />
+                <link rel="stylesheet" href="/assets/css/super-classes.css" />
+                <link rel="stylesheet" href="/assets/css/style.css" />
+                <link rel="stylesheet" href="/assets/css/mobile.css" />
             </Head>
             <body>
                 <Main />
                 <NextScript />
+
+                <Script src="assets/js/wow.js" />
+                <script> new WOW().init(); </script>
+                <Script src=":assets/js/jquery-3.6.0.min.js" strategy="lazyOnload"/> 
+                <Script src=" https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.js"  strategy="lazyOnload" />
+                <Script src="/assets/js/popper.min.js" strategy="lazyOnload" /> 
+                <Script src="/assets/js/bootstrap.min.js"  strategy="lazyOnload" /> 
+                <Script src="/assets/js/custom-script.js" strategy="lazyOnload" />
+                <Script src="/assets/js/contact-form.js" strategy="lazyOnload" />
 
             </body>
           </Html>
